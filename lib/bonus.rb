bc4cc78f420015c0d295e0b3b971a0ff7e5ac04f
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-17 23:54:05 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-epic_tragedy[:status] = "dead"
-epic_tragedy[:status] = "dead"
+epic_tragedy[:status][:hero][:status] = "dead"
+epic_tragedy[:status][:hero][:status] = "dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
